f630596c51a2715138a0cef6c0b8c532614dff1c
DarGrz <d.grzegorczyk@outlook.com>
2022-04-18 14:02:03 +0200

    Finish

--- main.js
+++ main.js
@@ -8,4 +8,36 @@ let body = selectElement('body');
 
 menuToggler.addEventListener('click', function() {
     body.classList.toggle('open');
-})
+});
+
+/* Scroll reveal */
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    delay: 300,
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
